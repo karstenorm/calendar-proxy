487b2b1d0044ffc499381da85a6a1cd7f47f6c5a
karstenorm <ok@>
2026-01-06 11:44:23 +0100

    ok

--- process_calendar.py
+++ process_calendar.py
@@ -4,6 +4,23 @@ with open("original.ics", "rb") as file:
     raw_data = file.read()
 
 cal = Calendar.from_ical(raw_data)
-print(cal)
 
+for event in cal.walk("VEVENT"):
+    summary = event.get("SUMMARY")
+    if "Emnekode" not in summary:
+        continue
+
+    summary_text = str(summary)
+    new_summary = summary_text.replace(
+        "Emnekode: ", ""
+    ).replace(
+        "Emnenavn: ", ""
+    ).replace(". ", " - ")
+    event["SUMMARY"] = new_summary
+
+raw_output = cal.to_ical()
+
+with open("processed.ics", "wb") as file:
+    file.write(raw_output)
+    print("file written")
 
