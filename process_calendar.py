bf437bbe97a86f37c5d95062ce74d63eb326ad30
karstenorm <ok@>
2026-03-24 18:09:49 +0100

    Update ics

--- process_calendar.py
+++ process_calendar.py
@@ -21,7 +21,7 @@ for event in cal.walk("VEVENT"):
 
     parts = summary_text.split(". ")
     code = parts[0].replace("Emnekode:", "").strip()  # remove "Emnekode"
-    name = parts[1].replace("Emnenavn:", "").strip()              # remove "Emnenavn"
+    name = parts[1].replace("Emnenavn:", "").strip()  # remove "Emnenavn"
 
     new_summary = f"{name} ({code})"  # new format
 
